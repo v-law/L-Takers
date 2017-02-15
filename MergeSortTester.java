43b00111e527e135e9c52be975259e7ebaa590d4
Dimitriy Leksanov <Dimitriy@Alekseys-MBP.fios-router.home>
2017-02-14 21:29:54 -0500

    Seemingly-working Tester with limited cases; updated README

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -1,11 +1,22 @@
+
+//Team L-Takers
+//Kristin Lin, Vernita Lawren, Dimitriy Leksanov
+//APCS2 pd1
+//HW07 -- What Does the Data Say?
+//2017-02-14
+
 /*======================================
   class MergeSortTester
 
   ALGORITHM:
-  <INSERT YOUR DISTILLATION OF ALGO HERE>
+  Continuously splits deck in half until each deck consists of one card. Cards
+  are then merged back together with their other half by continuously comparing 
+  the first cards of each deck and placing the lesser one into a new deck. 
+  Comparing and merging continues until the deck is whole again. This whole 
+  process features a binary tree being created then collapsing.
 
   BIG-OH CLASSIFICATION OF ALGORITHM:
-  <INSERT YOUR EXECUTION TIME CATEGORIZATION OF MERGESORT HERE>
+  O(nlog(n))
 
   Mean execution times for dataset of size n:
   Batch size: <# of times each dataset size was run>
@@ -19,8 +30,34 @@
   <INSERT YOUR RESULTS ANALYSIS HERE>
   ======================================*/
 
-public class MergeSortTester 
-{
+import java.util.ArrayList;
+
+public class MergeSortTester {
+
+    public static int[] crtDeck(int numCards){
+	int[] deck = new int[numCards];
+	for (int x = 0; x < numCards; x++){
+	    deck[x] = (int)(Math.random()*100);
+	}
+	return deck;
+    }
+
+    /*
+public static void runSort(int[] deck){
+MergeSort.sort(deck);
+}
+    */
+
+    //finds mean of execution times
+    public static long avgTime(ArrayList<Long> data) {
+	long total = 0;
+	data.remove(0); //for some reason, the first element is outlandishly big
+	for (long x : data) {
+	    System.out.println(x);
+	    total += x;
+	}
+	return total / data.size();
+    }
 
     /******************************
      * execution time analysis 
@@ -28,9 +65,58 @@ public class MergeSortTester
      *  YOUR APPARATUS FOR GENERATING EXECUTION 
      *  TIME DATA...>
      ******************************/
-    public static void main( String[] args ) 
-    {
+    public static void main( String[] args ) {
 
-    }//end main
+	//testing arrays with 1 integer
+	ArrayList<Long> timeData1 = new ArrayList();
+	for (int x = 0; x < 25; x++) {
+	    long execTime = System.nanoTime();
+	    MergeSort.sort(crtDeck(1));
+	    execTime = (System.nanoTime()) - execTime;
+	    timeData1.add(execTime);
+	}
+	System.out.println("Mean execution times for dataset of size 1: " + avgTime(timeData1));
 
+
+	//testing arrays with 10 integers
+	ArrayList<Long> timeData10 = new ArrayList();
+	for (int x = 0; x < 25; x++) {
+	    long execTime = System.nanoTime();
+	    MergeSort.sort(crtDeck(10));
+	    execTime = (System.nanoTime()) - execTime;
+	    timeData10.add(execTime);
+	}
+	System.out.println("Mean execution times for dataset of size 10: " + avgTime(timeData10));
+
+	//testing arrays with 100 integers
+	ArrayList<Long> timeData100 = new ArrayList();
+	for (int x = 0; x < 25; x++) {
+	    long execTime = System.nanoTime();
+	    MergeSort.sort(crtDeck(100));
+	    execTime = (System.nanoTime()) - execTime;
+	    timeData100.add(execTime);
+	}
+	System.out.println("Mean execution times for dataset of size 100: " + avgTime(timeData100));
+
+	//testing arrays with 1000 integers
+	ArrayList<Long> timeData1000 = new ArrayList();
+	for (int x = 0; x < 25; x++) {
+	    long execTime = System.nanoTime();
+	    MergeSort.sort(crtDeck(1000));
+	    execTime = (System.nanoTime()) - execTime;
+	    timeData1000.add(execTime);
+	}
+	System.out.println("Mean execution times for dataset of size 1000: " + avgTime(timeData1000));
+
+	//testing arrays with 2500 integers
+	ArrayList<Long> timeData2500 = new ArrayList();
+	for (int x = 0; x < 25; x++) {
+	    long execTime = System.nanoTime();
+	    MergeSort.sort(crtDeck(2500));
+	    execTime = (System.nanoTime()) - execTime;
+	    timeData2500.add(execTime);
+	}
+	System.out.println("Mean execution times for dataset of size 2500: " + avgTime(timeData2500));
+
+    }//end Main
 }//end class
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # L-Takers
+
+###ALGORITHM:
+#####  Continuously splits deck in half until each deck consists of one card. Cards
+#####  are then merged back together with their other half by continuously comparing 
+#####  the first cards of each deck and placing the lesser one into a new deck. 
+#####  Comparing and merging continues until the deck is whole again. This whole 
+#####  process features a binary tree being created then collapsing.
+
+###DATA COLLECTION:
+#####  Our data collection procedure consists of simple print statements.
+#####  Currently, our tester is capable of mergeSorting arrays of lengths 1, 10, 100, 1000, and 2500.
+#####  For each of these arrays, the sort() method of MergeSort is run 25 times, and the runtime of each run is printed. Runtimes are calculated through getting the difference in nanoTime between the beginning and end of each run.
+#####  As well, each runtime is recorded in an ArrayList, specified for each array length.
+#####  That is later used to calculate the average runtime of all the runs.
+######  NOTE: the first run is removed from the ArrayList, as it is always outlandishly high for 1-integer arrays, and skews the data.
+
+(https://docs.google.com/document/d/1wIJXsaQ4wn_kGQjuXBwVBvSov1giM5r_neP6kGksXmI/edit?ts=58a3aa67 "Our Document, featuring code and information")
